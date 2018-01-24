808b0ca3f97aad7e5e72c0112bbc7c79602e4eb2
Tristan Marshall <tam8217@g.rit.edu>
2018-01-24 09:40:13 -0500

    Added Basic movement, basic triggers

---
+++ Assets/Scripts/BasicTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTrigger : MonoBehaviour
+{
+    //Variables
+    public Material mat1;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        mat1.color = Color.red;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        mat1.color = Color.yellow;
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    //Variables
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y +1, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y -1, transform.position.z);
+        }
+    }
+}
