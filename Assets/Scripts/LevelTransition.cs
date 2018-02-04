749ddf60a4786a8aae1b6cfa5f5d8e2c226c8dc6
Tristan Marshall <tam8217@g.rit.edu>
2018-02-04 18:00:08 -0500

    Added in tutorial level, sprite based movement, tilemaps

---
+++ Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+    //Variables
+    public GameObject player;
+    public string nextLvl;
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Loading in the next scene
+        SceneManager.LoadScene(nextLvl);
+        //Resetting the position of the eplayer
+        player.transform.position = player.GetComponent<Sprt_Movement>().startPos;
+
+        
+    }
+}
---
+++ Assets/Scripts/Sprt_Movement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprt_Movement : MonoBehaviour
+{
+
+    public Vector3 startPos;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        //Setting the starting position
+        startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        DontDestroyOnLoad(this);
+        //Checking inputs
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //If there is nothing in front of the object
+            if (Physics2D.Raycast(transform.position, transform.up, transform.up.magnitude).collider == null)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (Physics2D.Raycast(transform.position, -transform.up, transform.up.magnitude).collider == null)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (Physics2D.Raycast(transform.position, -transform.right, transform.right.magnitude).collider == null)
+            {
+                transform.position = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (Physics2D.Raycast(transform.position, transform.right, transform.right.magnitude).collider == null)
+            {
+                transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+            }
+        }
+    }
+}
