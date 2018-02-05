cfebeac4d8c62daa53a3cdc69b635824b01c511f
David <Therobodavo@gmail.com>
2018-02-05 09:54:45 -0500

    Updated Scanner with delay

--- Assets/Scripts/Scanner.cs
+++ Assets/Scripts/Scanner.cs
@@ -16,6 +16,8 @@ public class Scanner : MonoBehaviour {
     List<GameObject> blocks;
     List<GameObject> objsFound;
     public GameObject whiteBlock;
+    public float secDelay = 2;
+    float timeLastEnded = -5;
 
 	// Use this for initialization
 	void Start ()
@@ -36,7 +38,7 @@ public class Scanner : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.L))
+        if (Input.GetKeyDown(KeyCode.L) && Time.time - timeLastEnded > (secDelay + 1))
         {
             scanning = true;
         }
@@ -70,6 +72,7 @@ public class Scanner : MonoBehaviour {
                     if(obj.GetComponent<Collider>().bounds.Intersects(GetComponent<Collider>().bounds) && objsFound.IndexOf(obj) == -1)
                     {
                         whiteBlock.transform.position = obj.transform.position;
+                        whiteBlock.transform.position += new Vector3(0, 0, -1);
                         whiteBlock.transform.localScale = obj.transform.localScale;
                         blocks.Add(Instantiate(whiteBlock));
                         objsFound.Add(obj);
@@ -80,6 +83,7 @@ public class Scanner : MonoBehaviour {
                     if(obj.GetComponent<Collider>().bounds.Intersects(GetComponent<Collider>().bounds) && objsFound.IndexOf(obj) == -1)
                     {
                         whiteBlock.transform.position = obj.transform.position;
+                        whiteBlock.transform.position += new Vector3(0, 0, -1);
                         whiteBlock.transform.localScale = obj.transform.localScale;
                         blocks.Add(Instantiate(whiteBlock));
                         objsFound.Add(obj);
@@ -90,6 +94,7 @@ public class Scanner : MonoBehaviour {
                     if(obj.GetComponent<Collider>().bounds.Intersects(GetComponent<Collider>().bounds) && objsFound.IndexOf(obj) == -1)
                     {
                         whiteBlock.transform.position = obj.transform.position;
+                        whiteBlock.transform.position += new Vector3(0, 0, -1);
                         whiteBlock.transform.localScale = obj.transform.localScale;
                         blocks.Add(Instantiate(whiteBlock));
                         objsFound.Add(obj);
@@ -98,12 +103,16 @@ public class Scanner : MonoBehaviour {
             }
             else
             {
-                foreach(GameObject b in blocks)
-                {
-                    Destroy(b);
-                }
+                timeLastEnded = Time.time;
                 scanning = false;
                 scanStarted = false;
+            }
+        }
+        if (Time.time - timeLastEnded >= secDelay && !scanning)
+        {
+            foreach (GameObject b in blocks)
+            {
+                Destroy(b);
             }
         }
     }
