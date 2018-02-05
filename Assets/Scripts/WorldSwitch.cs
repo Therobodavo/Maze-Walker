e3840038711dc430c07b5cf9b163f784351ebe6b
Tristan Marshall <tam8217@g.rit.edu>
2018-02-05 09:56:55 -0500

    Added potential death tile scripts

--- Assets/Scripts/WorldSwitch.cs
+++ Assets/Scripts/WorldSwitch.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class WorldSwitch : MonoBehaviour
 {
@@ -11,16 +12,30 @@ public class WorldSwitch : MonoBehaviour
     //Setting active variables
     private bool overAct;
     private bool underAct;
+
+    public Tilemap overMap;
+    public Tilemap underMap;
+
+    private GameObject player;
+    private Vector3Int playerInt;
+
+    private Vector3 initPlayerPos;
 	// Use this for initialization
 	void Start ()
     {
         overAct = true;
         underAct = false;
+
+        player = GameObject.Find("Player");
+        initPlayerPos = player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        playerInt = new Vector3Int((int)(player.transform.position.x +.5), (int)(player.transform.position.y +.5), (int)player.transform.position.z);
+
+        Debug.Log(overMap.GetTile(playerInt));
 		//Getting input to change the worlds
         if(Input.GetKeyDown(KeyCode.E))
         {
@@ -30,6 +45,28 @@ public class WorldSwitch : MonoBehaviour
 
             overWorld.SetActive(overAct);
             underWorld.SetActive(underAct);
+            /*
+            //Checking for player death 
+            if(overAct)
+            {
+                if(overMap.GetTile(playerInt))
+                {
+                    player.transform.position = initPlayerPos;
+                }
+            }
+            else if (overAct)
+            {
+                if (underMap.GetTile(playerInt))
+                {
+                    player.transform.position = initPlayerPos;
+                }
+            }
+            */
         }
 	}
+
+    public bool CheckTile(ITilemap itm, Vector3Int pos)
+    {
+        return itm.GetTile(pos) == this;
+    }
 }
