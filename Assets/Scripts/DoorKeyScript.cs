ad2d62ecd64ee8b02becc7e3ed339335dcb5ade4
mro5772 <mro5772@.g.rit.edu>
2018-02-15 19:34:20 -0800

    fixing bug

--- Assets/Scripts/DoorKeyScript.cs
+++ Assets/Scripts/DoorKeyScript.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class DoorKeyScript : MonoBehaviour
 {
+    private GameObject[] player;
 
     public List<GameObject> doors = new List<GameObject>();
 	// Use this for initialization
 	void Start ()
     {
-		
-	}
+
+        player = GameObject.FindGameObjectsWithTag("Player");
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -20,16 +22,22 @@ public class DoorKeyScript : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        //Looping through the connected objects and disabling them
-        foreach(GameObject go in doors)
+        BoxCollider pCollide = (BoxCollider)player[0].GetComponent("BoxCollider");
+        if(pCollide == other)
         {
-            /*go.GetComponent<MeshRenderer>().enabled = false;
-            go.GetComponent<BoxCollider2D>().enabled = false;
-            */
-            go.SetActive(false);
+            //Looping through the connected objects and disabling them
+            foreach (GameObject go in doors)
+            {
+                /*go.GetComponent<MeshRenderer>().enabled = false;
+                go.GetComponent<BoxCollider2D>().enabled = false;
+                */
+                go.SetActive(false);
+            }
+
+            //Disabling the key
+            this.gameObject.SetActive(false);
         }
 
-        //Disabling the key
-        this.gameObject.SetActive(false);
+      
     }
 }
